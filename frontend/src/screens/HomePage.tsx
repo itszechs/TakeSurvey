22854b50e0b281fb585d96b0274a5323fe175c7a
zechs <vijay.vky2002@gmail.com>
2023-01-29 23:20:03 +0530

    added options

--- frontend/src/screens/HomePage.tsx
+++ frontend/src/screens/HomePage.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import {
     Button, Card,
     Form, FormGroup,
     Input, Label
 } from "reactstrap";
+
 import "./HomePage.css";
 
 export default function HomePage() {
+    const [numberOfOptions, setNumberOfOptions] = useState(2);
+
     return (
         <div className="home-page">
             <h3 className="app-name" >TakeSurvery</h3>
@@ -21,6 +25,21 @@ export default function HomePage() {
                             placeholder="Type your question here..."
                             type="text" />
                         <Label className="label" for="options">Options</Label>
+                        {[...Array(numberOfOptions)].map((_, index) => (
+                            <div className="options-container" key={index} >
+                                <Input
+                                    className="option-input"
+                                    id={`option-${index}`}
+                                    name={`option-${index}`}
+                                    placeholder={`Option ${index + 1}`}
+                                    type="text" />
+                                <Button
+                                    className="delete-button"
+                                    color="danger"
+                                >Delete</Button>
+                            </div>
+                        ))
+                        }
                     </FormGroup>
                 </Form>
             </Card>
