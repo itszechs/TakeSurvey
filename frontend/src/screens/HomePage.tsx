d0142126064e4956d3aaa81bc1193383b6797aa6
zechs <vijay.vky2002@gmail.com>
2023-01-29 22:56:54 +0530

    basic home page

--- frontend/src/screens/HomePage.tsx
+++ frontend/src/screens/HomePage.tsx
@@ -2,8 +2,9 @@ import "./HomePage.css";
 
 export default function HomePage() {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className="home-page">
+            <h3 className="app-name" >TakeSurvery</h3>
+            <p className="app-description">TakeSurvery is a simple survey app that allows you to create and share surveys with your friends.</p>
         </div>
     );
 }
