ac13b2d015971ca16d6ec9b86b19f8879e79a128
zechs <vijay.vky2002@gmail.com>
2023-01-29 23:15:03 +0530

    designed survey form

--- frontend/src/screens/HomePage.tsx
+++ frontend/src/screens/HomePage.tsx
@@ -1,3 +1,8 @@
+import {
+    Button, Card,
+    Form, FormGroup,
+    Input, Label
+} from "reactstrap";
 import "./HomePage.css";
 
 export default function HomePage() {
@@ -5,6 +10,21 @@ export default function HomePage() {
         <div className="home-page">
             <h3 className="app-name" >TakeSurvery</h3>
             <p className="app-description">TakeSurvery is a simple survey app that allows you to create and share surveys with your friends.</p>
-        </div>
+            <Card outline className="form-card">
+                <h5>Create a new survey</h5>
+                <Form>
+                    <FormGroup>
+                        <Label className="label" for="question">Question</Label>
+                        <Input
+                            id="question"
+                            name="question"
+                            placeholder="Type your question here..."
+                            type="text" />
+                        <Label className="label" for="options">Options</Label>
+                    </FormGroup>
+                </Form>
+            </Card>
+            <Button className="submit-button" color="primary">Create</Button>
+        </div >
     );
 }
