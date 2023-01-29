532306e456c9392a3d50bb390d34ebbaa995e5e6
zechs <vijay.vky2002@gmail.com>
2023-01-29 22:19:09 +0530

    added poll router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,43 @@
+import dotenv from "dotenv";
+
 import express, { Request, Response, NextFunction } from "express";
+import { connectToDatabase } from "./services/database";
+import { pollRouter } from "./routes/poll";
 
-const app = express();
+// Load environment variables from the .env file, where the ATLAS_URI is configured
+dotenv.config();
 
-app.listen(5500, () => {
-    console.log(`Server running at http://localhost:5500`);
-});
+const { ATLAS_URI } = process.env;
 
-// Health check route
-app.get("/", (_req: Request, res: Response) => {
-    res.send({
-        message: "TakeSurvey api is running!",
-    });
-});
+if (!ATLAS_URI) {
+    console.error("No ATLAS_URI environment variable has been defined in config.env");
+    process.exit(1);
+}
 
+connectToDatabase(ATLAS_URI)
+    .then(() => {
+        const app = express();
 
-// Invalid route message
-app.use((_req: Request, res: Response, _next: NextFunction) => {
-    res.status(404).send({
-        error: "Not Found",
-    });
-});
+        app.listen(5500, () => {
+            console.log(`Server running at http://localhost:5500`);
+        });
+
+        // Health check route
+        app.get("/", (_req: Request, res: Response) => {
+            res.send({
+                message: "TakeSurvey api is running!",
+            });
+        });
+
+        // Poll routes
+        app.use("/api/v1/poll", pollRouter);
+
+        // Invalid route message
+        app.use((_req: Request, res: Response, _next: NextFunction) => {
+            res.status(404).send({
+                error: "Not Found",
+            });
+        });
+
+    })
+    .catch(error => console.error(error));
